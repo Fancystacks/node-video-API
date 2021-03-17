54964b32adf4cc956965c0b445f80dbe69e82e4d
Fancy Stacks <fancystacks@protonmail.com>
2021-03-16 22:44:15 -0600

    Setting up Express routes 🍿

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const courses = [ 
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+];
+
 app.get('/', (req, res) => {
     res.send('Henlo World');
 });
 
 app.get('/api/courses' , (req, res) => {
-    res.send([1 , 2 , 3]);
+    res.send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) => {
-b
+    const course = courses.find(c => c.id === parseInt(req.params.id));
 });
-
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
