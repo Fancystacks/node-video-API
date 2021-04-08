8d66b805a1c38e1e85da5c4f998a877a95bf19c0
Fancy Stacks <fancystacks@protonmail.com>
2021-04-08 01:19:10 -0600

    MongoDB schema 🍿

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true })
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Couldnt connect to mongoDB'));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
